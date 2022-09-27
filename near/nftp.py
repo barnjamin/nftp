56153a0dd732ccd683694f1b7a755fc865a29204
Josh Siegel <jsiegel@jumptrading.com>
2022-09-27 14:35:13 -0500

    master: save state

--- near/nftp.py
+++ near/nftp.py
@@ -2,7 +2,7 @@
 
 #  sudo dnf install fuse-devel
 #  python3 -m pip install fuse-python
-#  python3 nftp-near.py  ~/mnt
+#  python3 nftp.py  ~/mnt
 # .
 # .
 # .
@@ -23,6 +23,7 @@ except ImportError:
 import fuse
 from fuse import Fuse
 
+import logging
 
 if not hasattr(fuse, '__version__'):
     raise RuntimeError("your fuse-py doesn't know of fuse.__version__, probably it's too old.")
@@ -31,6 +32,12 @@ fuse.fuse_python_api = (0, 2)
 
 fuse.feature_assert('stateful_files', 'has_init')
 
+logger = logging.getLogger('spam_application')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('/home/jsiegel/spam.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 
 def flag2mode(flags):
     md = {os.O_RDONLY: 'rb', os.O_WRONLY: 'wb', os.O_RDWR: 'wb+'}
@@ -43,102 +50,73 @@ def flag2mode(flags):
 
 
 class Xmp(Fuse):
-
     def __init__(self, *args, **kw):
-
         Fuse.__init__(self, *args, **kw)
-
-        # do stuff to set up your filesystem here, if you want
-        #import thread
-        #thread.start_new_thread(self.mythread, ())
         self.root = '/'
 
-#    def mythread(self):
-#
-#        """
-#        The beauty of the FUSE python implementation is that with the python interp
-#        running in foreground, you can have threads
-#        """
-#        print "mythread: started"
-#        while 1:
-#            time.sleep(120)
-#            print "mythread: ticking"
-
     def getattr(self, path):
+        logger.info(["getattr", path])
         return os.lstat("." + path)
 
     def readlink(self, path):
+        logger.info("readlink")
         return os.readlink("." + path)
 
     def readdir(self, path, offset):
+        logger.info(["readdir", path, offset])
         for e in os.listdir("." + path):
             yield fuse.Direntry(e)
 
     def unlink(self, path):
+        logger.info("unlink")
         os.unlink("." + path)
 
     def rmdir(self, path):
+        logger.info("rmdir")
         os.rmdir("." + path)
 
     def symlink(self, path, path1):
+        logger.info("symlink")
         os.symlink(path, "." + path1)
 
     def rename(self, path, path1):
+        logger.info("rename")
         os.rename("." + path, "." + path1)
 
     def link(self, path, path1):
+        logger.info("link")
         os.link("." + path, "." + path1)
 
     def chmod(self, path, mode):
+        logger.info("chmod")
         os.chmod("." + path, mode)
 
     def chown(self, path, user, group):
+        logger.info("chown")
         os.chown("." + path, user, group)
 
     def truncate(self, path, len):
+        logger.info("truncate")
         f = open("." + path, "a")
         f.truncate(len)
         f.close()
 
     def mknod(self, path, mode, dev):
+        logger.info("mknod")
         os.mknod("." + path, mode, dev)
 
     def mkdir(self, path, mode):
+        logger.info("mkdir")
         os.mkdir("." + path, mode)
 
     def utime(self, path, times):
+        logger.info("utime")
         os.utime("." + path, times)
 
-#    The following utimens method would do the same as the above utime method.
-#    We can't make it better though as the Python stdlib doesn't know of
-#    subsecond preciseness in acces/modify times.
-#  
-#    def utimens(self, path, ts_acc, ts_mod):
-#      os.utime("." + path, (ts_acc.tv_sec, ts_mod.tv_sec))
-
     def access(self, path, mode):
+        logger.info(["access", path, mode])
         if not os.access("." + path, mode):
             return -EACCES
-
-#    This is how we could add stub extended attribute handlers...
-#    (We can't have ones which aptly delegate requests to the underlying fs
-#    because Python lacks a standard xattr interface.)
-#
-#    def getxattr(self, path, name, size):
-#        val = name.swapcase() + '@' + path
-#        if size == 0:
-#            # We are asked for size of the value.
-#            return len(val)
-#        return val
-#
-#    def listxattr(self, path, size):
-#        # We use the "user" namespace to please XFS utils
-#        aa = ["user." + a for a in ("foo", "bar")]
-#        if size == 0:
-#            # We are asked for size of the attr list, ie. joint size of attrs
-#            # plus null separators.
-#            return len("".join(aa)) + len(aa)
-#        return aa
 
     def statfs(self):
         """
@@ -159,6 +137,7 @@ class Xmp(Fuse):
             - f_ffree - nunber of free file inodes
         """
 
+        logger.info("statfs")
         return os.statvfs(".")
 
     def fsinit(self):
@@ -167,6 +146,7 @@ class Xmp(Fuse):
     class XmpFile(object):
 
         def __init__(self, path, flags, *mode):
+            logger.info(["XmpFile__init__", path, flags, *mode])
             self.file = os.fdopen(os.open("." + path, flags, *mode),
                                   flag2mode(flags))
             self.fd = self.file.fileno()
@@ -176,6 +156,8 @@ class Xmp(Fuse):
                 self.iolock = Lock()
 
         def read(self, length, offset):
+            logger.info(["XmpFileRead", length, offset])
+
             if self.iolock:
                 self.iolock.acquire()
                 try:
@@ -187,6 +169,7 @@ class Xmp(Fuse):
                 return os.pread(self.fd, length, offset)
 
         def write(self, buf, offset):
+            logger.info("XmpFileWrite")
             if self.iolock:
                 self.iolock.acquire()
                 try:
@@ -199,6 +182,7 @@ class Xmp(Fuse):
                 return os.pwrite(self.fd, buf, offset)
 
         def release(self, flags):
+            logger.info(["XmpFileRelease", flags])
             self.file.close()
 
         def _fflush(self):
@@ -206,6 +190,7 @@ class Xmp(Fuse):
                 self.file.flush()
 
         def fsync(self, isfsyncfile):
+            logger.info("XmpFileFSync")
             self._fflush()
             if isfsyncfile and hasattr(os, 'fdatasync'):
                 os.fdatasync(self.fd)
@@ -213,17 +198,21 @@ class Xmp(Fuse):
                 os.fsync(self.fd)
 
         def flush(self):
+            logger.info("XmpFileFlush")
             self._fflush()
             # cf. xmp_flush() in fusexmp_fh.c
             os.close(os.dup(self.fd))
 
         def fgetattr(self):
+            logger.info("XmpFileFGetAttr")
             return os.fstat(self.fd)
 
         def ftruncate(self, len):
+            logger.info("XmpFileTruncate")
             self.file.truncate(len)
 
         def lock(self, cmd, owner, **kw):
+            logger.info(["XmpFileLock", cmd, owner])
             # The code here is much rather just a demonstration of the locking
             # API than something which actually was seen to be useful.
 
@@ -265,11 +254,8 @@ class Xmp(Fuse):
 
 
     def main(self, *a, **kw):
-
         self.file_class = self.XmpFile
-
         return Fuse.main(self, *a, **kw)
-
 
 def main():
 
@@ -290,7 +276,7 @@ Userspace nullfs-alike: mirror the filesystem tree from some point on.
         if server.fuse_args.mount_expected():
             os.chdir(server.root)
     except OSError:
-        print("can't enter root of underlying filesystem", file=sys.stderr)
+        logger.info("can't enter root of underlying filesystem", file=sys.stderr)
         sys.exit(1)
 
     server.main()
